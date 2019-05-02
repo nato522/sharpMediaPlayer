a52a91c75c3c3cdc091d58c6ddfba084a582997b
Mario <mario.lopez.batres@gmail.com>
2019-05-02 12:51:06 +0300

    Code functioning

--- pdfHandler/pdfHandler.ino
+++ pdfHandler/pdfHandler.ino
@@ -27,10 +27,9 @@ const int RESET_VALUE = 100;
 
 // LEDs
 const int RED_PIN = 13;
-const int YELLOW_PIN = 8;
-const int GREEN_PIN = 4;
-const int RED_PIN2 = 3; //Scrolling mode
-const int WHITE_PIN2 = 2; //Paging mode
+const int YELLOW_PIN = 11;
+const int GREEN_PIN = 9;
+const int WHITE_PIN = 4; // Scrolling -> OFF | Paging -> ON
 
 
 // variables
@@ -45,20 +44,18 @@ int RANGE0, RANGE1, RANGE2, RANGE3, RANGE4, RANGE5, RANGE6 = 0;
 bool scrollMode = true; // 0 = false || 1 = true
 
 // action functions
-void execAction(int action, int prevAction, int auxAction){
+void execAction(int action, int prevAction, int auxAction) {
   switch (action) {
     case STOP:
     case START:
-      if (action == prevAction){
+      if (action == prevAction) {
         scrollMode = !scrollMode;
-        if(scrollMode==true){
-        digitalWrite(RED_PIN2, HIGH);
-        digitalWrite(WHITE_PIN2, LOW);
-        Serial.println("Scrolling");
-        } else {
-        digitalWrite(RED_PIN2, LOW);
-        digitalWrite(WHITE_PIN2, HIGH);
-        Serial.println("Paging");
+        if (scrollMode == true) { //Scroll mode
+          digitalWrite(WHITE_PIN, LOW);
+          Serial.println("Scrolling");
+        } else {  // Paging mode
+          digitalWrite(WHITE_PIN, HIGH);
+          Serial.println("Paging");
         }
         digitalWrite(RED_PIN, LOW);
         digitalWrite(YELLOW_PIN, HIGH);
@@ -67,36 +64,36 @@ void execAction(int action, int prevAction, int auxAction){
       }
       break;
     case PREVIOUS:
-      if (action == prevAction){
-        Serial.println((scrollMode==true) ? ("Scrolling") : ("Paging"));
+      if (action == prevAction) {
+        Serial.println((scrollMode == true) ? ("Scrolling UP") : ("Previous Page"));
         digitalWrite(RED_PIN, HIGH);
         digitalWrite(YELLOW_PIN, LOW);
         digitalWrite(GREEN_PIN, LOW);
-        if(scrollMode){
-          Keyboard.write(0xDA); // UP ARROW
-        }else{
-          Keyboard.write(0xD8); // Arrow left
+        if (scrollMode) {
+          Keyboard.write(0xDA); // UP ARROW to Scroll
+        } else {
+          Keyboard.write(0xD8); // Arrow left to Jump page
           delay(700);
         }
       }
       break;
     case NEXT:
-      if (action == prevAction){
-        Serial.println("NEXT");
+      if (action == prevAction) {
+        Serial.println((scrollMode == true) ? ("Scrolling DOWN") : ("Next Page"));
         digitalWrite(RED_PIN, LOW);
         digitalWrite(YELLOW_PIN, LOW);
         digitalWrite(GREEN_PIN, HIGH);
-        if(scrollMode){
-          Keyboard.write(0xD9); // DOWN ARROW
-        }else{
-          Keyboard.write(0xD7); // Arrow right
+        if (scrollMode) {
+          Keyboard.write(0xD9); // Down ARROW to Scroll
+        } else {
+          Keyboard.write(0xD7); // Arrow Right to Jump page
           delay(700);
         }
       }
       break;
     case SWIPEUP:
-      if ((action == prevAction)&&(action == auxAction)){
-        Serial.println("Zoom Out");
+      if ((action == prevAction) && (action == auxAction)) {
+        Serial.println("Zoom OUT");
         digitalWrite(RED_PIN, HIGH);
         delay(100);
         digitalWrite(RED_PIN, LOW);
@@ -109,27 +106,27 @@ void execAction(int action, int prevAction, int auxAction){
         delay(100);
         digitalWrite(GREEN_PIN, LOW);
         Keyboard.press(KEY_LEFT_GUI);
-          Keyboard.write(0x2F); // -
-          Keyboard.releaseAll();
+        Keyboard.write(0x2F); // -
+        Keyboard.releaseAll();
       }
       break;
     case SWIPEDOWN:
-      if ((action == prevAction)&&(action == auxAction)){
-        Serial.println((scrollMode==true) ? ("Scroll Down") : ("Zoom In"));
+      if ((action == prevAction) && (action == auxAction)) {
+        Serial.println("Zoom IN");
         digitalWrite(GREEN_PIN, HIGH);
         delay(100);
         digitalWrite(GREEN_PIN, LOW);
         delay(100);
         digitalWrite(YELLOW_PIN, HIGH);
-        delay(100); 
+        delay(100);
         digitalWrite(YELLOW_PIN, LOW);
         delay(100);
         digitalWrite(RED_PIN, HIGH);
         delay(100);
         digitalWrite(RED_PIN, LOW);
-          Keyboard.press(KEY_LEFT_GUI);
-          Keyboard.write(0x5D); // +
-          Keyboard.releaseAll();
+        Keyboard.press(KEY_LEFT_GUI);
+        Keyboard.write(0x5D); // +
+        Keyboard.releaseAll();
       }
       break;
     default:
@@ -137,61 +134,63 @@ void execAction(int action, int prevAction, int auxAction){
   }
 }
 
-void detectArea(int distance){
-  if (distance > RANGE0 && distance < RANGE2){
+void detectArea(int distance) {
+  if (distance > RANGE0 && distance < RANGE2) {
     execAction(PREVIOUS, PREV_ACTION, AUX_ACTION);
     PREV_ACTION = PREVIOUS;
   }
-  else if (distance >= RANGE2 && distance < RANGE4){
-    if (scrollMode){
+  else if (distance >= RANGE2 && distance < RANGE4) {
+    if (scrollMode) {
       execAction(START, PREV_ACTION, AUX_ACTION);
       PREV_ACTION = START;
     }
-    else{
+    else {
       execAction(STOP, PREV_ACTION, AUX_ACTION);
       PREV_ACTION = STOP;
     }
   }
-  else if (distance >= RANGE4 && distance < RANGE6){
+  else if (distance >= RANGE4 && distance < RANGE6) {
     execAction(NEXT, PREV_ACTION, AUX_ACTION);
     PREV_ACTION = NEXT;
   }
-  else{
+  else {
     //Serial.println("Out of range");
     PREV_ACTION = RESET_VALUE;
     AUX_ACTION = RESET_VALUE;
   }
 }
 
-void defineAction(int change, int distance){
-  if (change > 0){
+void defineAction(int change, int distance) {
+  if (change > 0) {
     execAction(SWIPEUP, PREV_ACTION, AUX_ACTION);
     if (SWIPEUP == PREV_ACTION)
       AUX_ACTION = SWIPEUP;
     else
       PREV_ACTION = SWIPEUP;
   }
-  else if (change == 0){
+  else if (change == 0) {
     detectArea(distance);
   }
-  else{
+  else {
     execAction(SWIPEDOWN, PREV_ACTION, AUX_ACTION);
-    if (SWIPEDOWN == PREV_ACTION){
+    if (SWIPEDOWN == PREV_ACTION) {
       AUX_ACTION = SWIPEDOWN;
     }
-    else{
+    else {
       PREV_ACTION = SWIPEDOWN;
     }
   }
 }
 
-void setup(){
+void setup() {
   Serial.begin(9600); //Enable the serial comunication
   Keyboard.begin();
   pinMode(RED_PIN, OUTPUT);
   pinMode(YELLOW_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
-  switch(MODEL){
+  pinMode(WHITE_PIN, OUTPUT);
+
+  switch (MODEL) {
     case SHORTDISTANCE:
       RANGE0 = 5;
       RANGE1 = 15;
@@ -215,52 +214,52 @@ void setup(){
   }
 }
 
-void loop(){
+void loop() {
   delay(150);
   int distance = sensor.getDistance(); //Calculate the distance in centimeters and store the value in a variable
   digitalWrite(RED_PIN, LOW);
   digitalWrite(YELLOW_PIN, LOW);
   digitalWrite(GREEN_PIN, LOW);
-  
-  if(distance < RANGE0){
+
+  if (distance < RANGE0) {
     section = 0;
     change = section - prevSection;
     defineAction(change, distance);
   }
-  else if(distance >= RANGE0 && distance <= RANGE1){
+  else if (distance >= RANGE0 && distance <= RANGE1) {
     section = 1;
     change = section - prevSection;
     defineAction(change, distance);
   }
-  else if(distance > RANGE1 && distance <= RANGE2){
+  else if (distance > RANGE1 && distance <= RANGE2) {
     section = 2;
     change = section - prevSection;
     defineAction(change, distance);
   }
-  else if(distance > RANGE2 && distance <= RANGE3){
+  else if (distance > RANGE2 && distance <= RANGE3) {
     section = 3;
     change = section - prevSection;
     defineAction(change, distance);
   }
-  else if(distance >= RANGE3 && distance <= RANGE4){
+  else if (distance >= RANGE3 && distance <= RANGE4) {
     section = 4;
     change = section - prevSection;
     defineAction(change, distance);
   }
-  else if(distance >= RANGE4 && distance <= RANGE5){
+  else if (distance >= RANGE4 && distance <= RANGE5) {
     section = 5;
     change = section - prevSection;
     defineAction(change, distance);
   }
-  else if(distance >= RANGE5 && distance <= RANGE6){
+  else if (distance >= RANGE5 && distance <= RANGE6) {
     section = 6;
     change = section - prevSection;
     defineAction(change, distance);
   }
-  else if (distance > RANGE6){
-   section = 7;
-   change = section - prevSection;
-   defineAction(change, distance);
+  else if (distance > RANGE6) {
+    section = 7;
+    change = section - prevSection;
+    defineAction(change, distance);
   }
 
   prevSection = section;
